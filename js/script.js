9894ebcb79450d4145c8f83220644f0434fe0a68
MICHAEL_ROSENSTRAUS <rrosenstrausmichael@gmail.com>
2024-09-30 08:46:34 -0400

    done

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $('document').ready(function(){
-$('.swatch').on('click', function(){
+$('.swatch').on('mouseover', function(){
     $('#product-photo').attr('src', 'img/chair-' + $(this).attr('id') + '@2x.jpg');
-
-
+    $('swatch').removeClass('selected')
+    $(this).addClass('selected');
 });
 });
